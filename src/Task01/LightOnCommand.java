6f1839130f8be54cf1e13f70872ef53f94281045
Shami <yashodashaminda249@gmail.com>
2024-08-23 01:56:59 +0530

    Implement LightOnCommand class

---
+++ src/Task01/LightOnCommand.java
@@ -0,0 +1,19 @@
+package Task01;
+
+public class LightOnCommand implements Command{
+    private Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+
+    public void execute() {
+        light.on();
+    }
+
+    public void undo() {
+        light.off();
+    }
+
+}
