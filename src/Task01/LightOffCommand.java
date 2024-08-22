5ec53788e211f39d9c0c04911e872eec30c64f10
Shami <yashodashaminda249@gmail.com>
2024-08-23 02:02:22 +0530

    Implement LightOffCommand class

---
+++ src/Task01/LightOffCommand.java
@@ -0,0 +1,18 @@
+package Task01;
+
+public class LightOffCommand implements Light{
+    private Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.off();
+    }
+
+    public void undo() {
+        light.on();
+    }
+
+}
