38d1767ef1d58e1ba2f1d72f4d9554ee3698fa8e
Shami <yashodashaminda249@gmail.com>
2024-08-23 11:11:20 +0530

    Square, Circle and Triangle class draw shapes

--- src/Task03/Circle.java
+++ src/Task03/Circle.java
@@ -3,6 +3,22 @@ package Task03;
 public class Circle implements Shape{
     @Override
     public void draw() {
+        int r = 10;
+        int diameter = 2 * r + 1;
 
+        for (int i = 0; i < diameter; i++) {
+            for (int j = 0; j < diameter; j++) {
+                int  x + y * y <= r * r + r * 0.8) {
+                    Sysx = i - r;
+                    int y = j - r;
+                    if (x *tem.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
+
+
--- src/Task03/Triangle.java
+++ src/Task03/Triangle.java
@@ -1,8 +1,19 @@
 package Task03;
 
-public class Triangle implements Shape{
+public class Triangle implements Shape {
     @Override
     public void draw() {
-
+        int height = 5;
+        int i;
+        for (i = 1; i <= height; i++) {
+            for (int j = 0; j < height - i; j++) {
+                System.out.print("  ");
+            }
+        }
+        for (int k = 0; k < 2 * i + 1; k++) {
+            System.out.print("* ");
+        }
+        System.out.println();
     }
 }
+
