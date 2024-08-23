9211911bdc03ca508b39c7c5b28cb0acac05a1f9
Shami <yashodashaminda249@gmail.com>
2024-08-23 09:44:05 +0530

    template method add on Beverages class

---
+++ src/Task02/Beverages.java
@@ -0,0 +1,25 @@
+package Task02;
+
+public abstract class Beverages {
+    private boolean wantsExtras;
+    public void setWantsExtras(boolean wantsExtras){
+        this.wantsExtras = wantsExtras;
+    }
+
+    public void  boilWater(){
+        System.out.println("Boiling water");
+    }
+    public void pourInCup(){
+        System.out.println("Pouring into cup ");
+    }
+
+    public final void finalTemplateMethod(){
+        boilWater();
+        brew();
+        pourInCup();
+        addCondiments();
+        if (wantsExtras){
+            addExtras();
+        }
+    }
+}
